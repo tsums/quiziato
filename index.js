9328b150917816cae035962e6a85028f78e97b5f
Trevor Summerfield <tresummerfield@gmail.com>
2015-09-12 14:23:57 -0400

    got sockets working, basically

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
+var http = require("http");
+var app = require("express")();
+var port = process.env.PORT || 5000;
+var server = http.Server(app);
+var io = require('socket.io')(server);
 
-app.use(express.static(__dirname + "/"))
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
 
-var server = http.createServer(app)
-server.listen(port)
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
 
-console.log("http server listening on %d", port)
+server.listen(port, function(){
+    console.log('listening on *:' + port);
+});
 
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
-
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
-
-  console.log("websocket connection open")
-
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
