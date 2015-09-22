641a531281c1e279151942d087ed1dd271498a6f
Trevor Summerfield <tresummerfield@gmail.com>
2015-09-22 15:05:54 -0400

    socket rooms...?

--- sockets.js
+++ sockets.js
@@ -10,10 +10,20 @@ module.exports.listen = function(server){
 
     var io = socketio.listen(server);
 
-    io.on('connection', function (socket) {
+    var classroom = io.of('/classroom');
+
+    classroom.on('connection', function (socket) {
         socket.emit('news', { hello: 'world' });
         socket.on('click', function (data) {
             console.log(data);
+        });
+
+        socket.on('attendance', function(data) {
+           if (data === true) {
+               socket.join('inClass');
+               classroom.to('inClass').emit('question', "What is the square root of 4?");
+               console.log('socket joining inClass');
+           }
         });
     });
 
