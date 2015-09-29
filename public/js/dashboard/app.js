9e62de0162604c7ef0f059727d8dd8184261e3d6
Trevor Summerfield <tresummerfield@gmail.com>
2015-09-29 15:37:53 -0400

    got basic angular, ng-socket working

--- public/js/dashboard/app.js
+++ public/js/dashboard/app.js
@@ -6,5 +6,26 @@
 var app = angular.module('dashboard', ['btford.socket-io']);
 
 
+app.factory('dashSocket', function(socketFactory) {
+    var socket = io.connect(window.location.origin + '/dashboard');
+    return socketFactory({
+        ioSocket: socket
+    });
+});
+
+app.controller('dashboardController', function(dashSocket) {
+
+    dashSocket.on('news', function (data) {
+        console.log(data);
+        socket.emit('click', { my: 'data' });
+    });
+    dashSocket.on('question', function(data) {
+        console.log(data);
+    });
+    dashSocket.on('cat', function(data) {
+        console.log(data);
+    });
+});
+
 
 
