4a3144757431f61dcbc5b66ff151dddfdbe3864f
Trevor Summerfield <tresummerfield@gmail.com>
2015-09-29 13:31:45 -0400

    should world

--- services/sockets.js
+++ services/sockets.js
@@ -47,7 +47,7 @@ var listen = function (server) {
         // TODO classroom namespace needs a bearer token authentication scheme.
         // TODO have Mike try sending headers and see if I can parse them.
 
-        winston.info(socket.request.headers);
+        winston.info(socket.request.headers.authorization);
         winston.info(socket.auth);
         next();
     });
