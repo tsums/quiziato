e6c222a05e45f5cd87f487830a755014164aaf67
Trevor Summerfield <tresummerfield@gmail.com>
2015-10-02 22:06:33 -0400

    question model, need to work on manager

--- models/question.js
+++ models/question.js
@@ -7,12 +7,19 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var QuestionOption = new Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true}
+});
+
 //TODO Schema
 var Question = new Schema({
-    title : {type: String},
-    prompt: {type: String},
-    author: {type: ObjectId}
-
+    title : {type: String, required: true},
+    prompt: {type: String, required: true},
+    course: {type: ObjectId, required: true},
+    author: {type: ObjectId, required: true},
+    options: [QuestionOption],
+    correctOption: {type: Number, required: true}
 });
 
 module.exports = mongoose.model('Question', Question);
