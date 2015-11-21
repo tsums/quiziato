bc61cf6c17c5faf7e6b499296b39f1656271634b
Trevor Summerfield <tresummerfield@gmail.com>
2015-11-21 15:09:51 -0500

    update

--- models/questionAssignment.js
+++ models/questionAssignment.js
@@ -7,11 +7,24 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var AssignmentAnswer = require('./assignmentAnswer');
+
 var QuestionAssignment = new Schema({
     question: {type: Schema.Types.ObjectId, required: true, ref: 'Question'},
     assignedAt: {type: Date, required: true},
     dueAt: {type: Date, required: true},
     graded: {type: Boolean, required: true, default: true}
+}, {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true }
 });
+
+// TODO virtual here that finds the percentages for the assignment
+QuestionAssignment.virtual('average').get(function () {
+
+
+
+});
+
 
 module.exports = mongoose.model('QuestionAssignment', QuestionAssignment);
